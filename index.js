f4a59e5aea5ff33a20d0ef8c6435b5177a1a66a5
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2023-12-04 13:41:26 -0700

    gitignore and index.js change to provoMEntalHealth

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const knex = require('knex') ({
         host: process.env.RDS_HOSTNAME || 'localhost',
         user: process.env.RDS_USERNAME || 'postgres',
         password: process.env.RDS_PASSWORD || 'admin',
-        database: process.env.RDS_DB_NAME || 'bucket_list', 
+        database: process.env.RDS_DB_NAME || 'ProvoMentalHealth', 
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
