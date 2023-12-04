0d37d7d314c68f05072760a8c3630fe9ac572b66
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2023-12-04 12:19:13 -0700

    index.js file updates

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+let app = express();
+
+let path = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:true}));
+
+const knex = require('knex') ({
+    client: 'pg',
+    connection: {
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        user: process.env.RDS_USERNAME || 'postgres',
+        password: process.env.RDS_PASSWORD || 'admin',
+        database: process.env.RDS_DB_NAME || 'bucket_list', 
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+});
+
+app.listen(port, () => console.log("Server is listening."));
