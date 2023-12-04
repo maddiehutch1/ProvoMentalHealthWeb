732a82aefc521f461c48e60b846e85c934047c80
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2023-12-04 16:51:48 -0700

    ebeanstalk cert change

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d provocityhealth.is404.net --nginx --agree-tos --email oliver.escobar1122@gmail.com
+sudo certbot -n -d ProvoSMU-2-10.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email oliver.escobar1122@gmail.com
