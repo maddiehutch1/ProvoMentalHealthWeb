4d9bd1464facf6e9cc3335b30b212973f3379c43
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2023-12-05 11:56:11 -0700

    changes to certification

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 sudo certbot -n -d provocityhealth.is404.net --nginx --agree-tos --email oliver.escobar1122@gmail.com
+sudo certbot -n -d intex2-10.is404.net --nginx --agree-tos --email oliver.escobar1122@gmail.com
