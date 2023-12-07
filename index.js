e49e8414dd30fe2c4d6d1356284c1634e16d063a
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2023-12-06 22:23:40 -0700

    removed a slash

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ app.get("/editemployee/:id", (req, res)=> {
    });
 });
 
-app.post("/editemployee", (req, res)=> {
-    knex("Login").where("LoginID", parseInt(req.body.Login)).update({
+app.post("editemployee", (req, res)=> {
+    knex("Login").where("LoginID", parseInt(req.body.LoginID)).update({
       LoginID: req.body.LoginID,
       Username: req.body.Username,
       Password: req.body.Password,
